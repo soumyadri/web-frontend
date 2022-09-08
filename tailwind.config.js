e51bc644e9ffb8bd331f1e6fb8e41dd596a450f3
Soumyadri Das <dadsoumyadri@gmail.com>
2022-09-08 23:50:05 +0530

    Header created

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {},
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
+    color: {
+      blue: '#f0f080',
+    },
+    spacing: {
+      '1p': '10px',
+      '2p': '20px',
+      '3p': '30px',
+      '4p': '40px',
+      '5p': '50px',
+      '6p': '60px',
+    }
+  },
+  plugins: [],
+}
